d445769d65d49857606404b9012666314f47b6d6
ankurkhandelwal <ankurkhandelwal08027@gmail.com>
2017-05-13 12:17:39 +0530

    first commit

--- socialApp/settings.py
+++ socialApp/settings.py
@@ -79,15 +79,24 @@ WSGI_APPLICATION = 'socialApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # DATABASES['default'] = dj_database_url.config()
+# DATABASES = {
+#     'default': { dj_database_url.config()
+#     }
+# }
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 try:
     from .local_settings import *
