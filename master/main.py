63f5127d3e075f7c88080faeae097db176064b43
Apple <ankurkhandelwal08027@gmail.com>
2018-01-09 12:21:25 +0530

    added algo

--- master/main.py
+++ master/main.py
@@ -1,7 +1,7 @@
 import googlemaps
 from datetime import datetime, timedelta, date
 import datetime as dt
-import json
+import json, sys
 
 class GoogleMaps():
     def __init__(self):
@@ -13,99 +13,149 @@ class GoogleMaps():
         return matrix
 
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-datetime', required=True)
-    parser.add_argument('-neareststop', required=True)
-    parser.add_argument('-finalstop', required=True)
-    parser.add_argument('-n_busses', required=True)
-    args = parser.parse_args()
-    try:
-        dt_obj = datetime.strptime(args.datetime, '%d.%m.%Y %H:%M:%S')
-        user_input_time = int(dt_obj.timestamp())
-        input_stop = str(args.neareststop)
-        # user_input_time = int(datetime.timestamp(datetime.now()))
-        input_final_stop = str(args.finalstop)
-        number_of_buses = int(args.n_busses)
-    except AttributeError:
-        print('Please input the variables in a correct format')
-        import sys
-        sys.exit(0)
+class Routes():
 
-    # dt_obj = datetime.strptime('07.01.2018 06:38:42', '%d.%m.%Y %H:%M:%S')
-    # user_input_time = float(dt_obj.timestamp())
-    # input_stop = str('Trilok Puri Blk-26(Gurudwara)')
-    # # user_input_time = int(datetime.timestamp(datetime.now()))
-    # input_final_stop = str('Pandav Nagar Police Station')
-    # number_of_buses = 5
+    def __init__(self,  user_input_time, input_first_stop, input_final_stop, n_busses):
+        self.user_input_time = user_input_time
+        self.input_first_stop = input_first_stop
+        self.input_final_stop = input_final_stop
+        self.n_busses = n_busses
+        try:
+            path = 'routes_dtc_cfati_new.json'
+            data = json.load(open(path))
+            data_U = data[0] # FORWARD ROUTE
+            data_D = data[1]  # FORWARD ROUTE
+            self.data = self.get_route_type(data_U, data_D)
+        except FileNotFoundError:
+            print('Please insert "routes_dtc_cfati.json" in the same folder where python script is ')
+            print('Exiting program ...')
+            sys.exit(0)
+        except AttributeError:
+            print('The data is not in the desired format.')
+            print('Exiting program ...')
+            sys.exit(0)
 
-    try:
-        path = 'routes_dtc_cfati.json'
-        data = json.load(open(path))
-        data = data[0]  # take only one route
-    except FileNotFoundError:
-        print('Please insert "routes_dtc_cfati.json" in the same folder where python script is ')
-    except AttributeError:
-        print('The data is not in the desired format.')
+    def get_route_type(self, data_U, data_D):
+        for i in range(len(data_U['stop_details'])):
+            if (self.input_first_stop == data_U['stop_details'][i]['stop_name']):
+                seq_start = int(data_U['stop_details'][i]['stop_seq'])
+            if (self.input_final_stop == data_U['stop_details'][i]['stop_name']):
+                seq_final = int(data_U['stop_details'][i]['stop_seq'])
+
+        route = None
+        if seq_start < seq_final:
+            A = (data_U['first_stop_lat'], data_U['first_stop_long'])
+            B = (data_U['last_stop_lat'], data_U['last_stop_long'])
+            route = data_U
+        else:
+            A = (data_D['first_stop_lat'], data_D['first_stop_long'])
+            B = (data_D['last_stop_lat'], data_D['last_stop_long'])
+            route = data_D
+        return route
+
+    def validate_variables(self):
+        try:
+            dt_obj = datetime.strptime(self.user_input_time, '%d.%m.%Y %H:%M:%S')
+            user_input_time = float(dt_obj.timestamp())
+        except AttributeError:
+            print('Datetime variable is not in the desired for format.'
+                  'The desired format is "%d.%m.%Y %H:%M:%S"')
+            print('Exiting program ...')
+            sys.exit(0)
+
+        try:
+            input_first_stop = str(self.input_first_stop).lower()
+            input_final_stop = str(self.input_final_stop).lower()
+        except AttributeError:
+            print('Stop names must be string values and must match the exact stop name')
+            print('Exiting program ...')
+            sys.exit(0)
+        try:
+            n_busses = int(self.n_busses)
+            if n_busses < 1:
+                raise ValueError('Number of busses must be at least one to determine a route')
+        except TypeError:
+            print('Number of busses must be an integer value')
+            print('Exiting program ...')
+            sys.exit(0)
+        return [user_input_time, input_first_stop, input_final_stop, n_busses]
 
 
-    all_stops = data['stop_details']
-    running_times = [x['start_time'] for x in data['running_frequency']]
+    def get_route_information(self, validated_variables):
+        user_input_time, input_stop, input_final_stop, number_of_buses = validated_variables
+        all_stops = self.data['stop_details']
+        running_times = [x['start_time'] for x in self.data['running_frequency']]
 
-    # ROUTE START AND END COORDINATES
-    A = (data['first_stop_lat'], data['first_stop_long'])
-    B = (data['last_stop_lat'], data['last_stop_long'])
+        # ROUTE START AND END COORDINATES
+        A = (self.data['first_stop_lat'], self.data['first_stop_long'])
+        B = (self.data['last_stop_lat'], self.data['last_stop_long'])
 
-    for nearest_stop in all_stops:
-        if (nearest_stop['stop_name'] == input_stop):
-            Y = (nearest_stop['stop_lat'] ,nearest_stop['stop_long'])
+        for nearest_stop in all_stops:
+            if (nearest_stop['stop_name'] == input_stop):
+                Y = (nearest_stop['stop_lat'], nearest_stop['stop_long'])
 
-        if (nearest_stop['stop_name'] == input_final_stop):
-            Z = (nearest_stop['stop_lat'], nearest_stop['stop_long'])
+            if (nearest_stop['stop_name'] == input_final_stop):
+                Z = (nearest_stop['stop_lat'], nearest_stop['stop_long'])
 
-    obj = GoogleMaps()
-    get_distance_from_A_Y = obj.get_minimum_distance(A, Y)
-    distace_A_Y = float(str(get_distance_from_A_Y['rows'][0]['elements'][0]['distance']['text']).split(' ')[0])
-    duration_A_Y = int(str(get_distance_from_A_Y['rows'][0]['elements'][0]['duration']['text']).split(' ')[0])
+        obj = GoogleMaps()
+        get_distance_from_A_Y = obj.get_minimum_distance(A, Y)
+        distace_A_Y = float(str(get_distance_from_A_Y['rows'][0]['elements'][0]['distance']['text']).split(' ')[0])
+        duration_A_Y = int(str(get_distance_from_A_Y['rows'][0]['elements'][0]['duration']['text']).split(' ')[0])
 
-    print('Distance from nearest stop to First stop: {} meter/km(s)'.format(distace_A_Y))
-    print('Time takes to reach from First stop to nearest user stop: {} min(s)'.format(duration_A_Y))
+        print('Distance from nearest stop to First stop: {} meter/km(s)'.format(distace_A_Y))
+        print('Time takes to reach from First stop to nearest user stop: {} min(s)'.format(duration_A_Y))
 
-    bus_must_leave_after = user_input_time - timedelta(minutes=duration_A_Y).total_seconds() # IN UNIX
-    bus_must_leave_after = datetime.fromtimestamp(bus_must_leave_after) # IN NORMAL TIME
-    bus_must_leave_after = datetime.strftime(bus_must_leave_after, '%H:%M:%S %p')
-    bus_must_leave_after = datetime.strptime(bus_must_leave_after, '%H:%M:%S %p').time() # PARSE ONLY TIME
-    print('According to user input time, the bus had to leave after: {}'.format(bus_must_leave_after))
+        bus_must_leave_after = user_input_time - timedelta(minutes=duration_A_Y).total_seconds()  # IN UNIX
+        bus_must_leave_after = datetime.fromtimestamp(bus_must_leave_after)  # IN NORMAL TIME
+        bus_must_leave_after = datetime.strftime(bus_must_leave_after, '%H:%M:%S %p')
+        bus_must_leave_after = datetime.strptime(bus_must_leave_after, '%H:%M:%S %p').time()  # PARSE ONLY TIME
+        print('According to user input time, the bus had to leave after: {}'.format(bus_must_leave_after))
 
-    bus_scheduled_time = []
-    multiple_buses = 0
-    for time in running_times:
-        time = datetime.strptime(time, '%H:%M:%S %p').time()
-        if (bus_must_leave_after<time):
-            bus_scheduled_time.append(time)
-            print('Scheduled buses left at: {}'.format(time))
-            multiple_buses += 1
-            if (multiple_buses == number_of_buses): break
+        bus_scheduled_time = []
+        multiple_buses = 0
+        for time in running_times:
+            time = datetime.strptime(time, '%H:%M:%S %p').time()
+            if (bus_must_leave_after < time):
+                bus_scheduled_time.append(time)
+                print('Scheduled buses left at: {}'.format(time))
+                multiple_buses += 1
+                if (multiple_buses == number_of_buses): break
 
-    all_schedules = []
-    for schedule in bus_scheduled_time:
-        print('Schedule for bus leaving at {}'.format(schedule))
-        time_bus_will_reach = (datetime.combine(dt.date(1,1,1),schedule) + timedelta(minutes=duration_A_Y)).time()
-        user_input_time_time = datetime.fromtimestamp(user_input_time).time()
-        ETA = datetime.combine(date.today(), time_bus_will_reach) - datetime.combine(date.today(), user_input_time_time)
-        print('Estimated time of arrival at the user nearest bus stop: {}'.format(ETA))
+        all_schedules = []
+        for schedule in bus_scheduled_time:
+            print('Schedule for bus leaving at {}'.format(schedule))
+            time_bus_will_reach = (datetime.combine(dt.date(1, 1, 1), schedule) + timedelta(minutes=duration_A_Y)).time()
+            user_input_time_time = datetime.fromtimestamp(user_input_time).time()
+            ETA = datetime.combine(date.today(), time_bus_will_reach) - datetime.combine(date.today(),
+                                                                                         user_input_time_time)
+            print('Estimated time of arrival at the user nearest bus stop: {}'.format(ETA))
 
-        get_distance_from_Y_Z = obj.get_minimum_distance(A, Z)
-        distace_Y_Z = float(str(get_distance_from_Y_Z['rows'][0]['elements'][0]['distance']['text']).split(' ')[0])
-        duration_Y_Z = int(str(get_distance_from_Y_Z['rows'][0]['elements'][0]['duration']['text']).split(' ')[0])
+            get_distance_from_Y_Z = obj.get_minimum_distance(Y, Z)
+            distace_Y_Z = float(str(get_distance_from_Y_Z['rows'][0]['elements'][0]['distance']['text']).split(' ')[0])
+            duration_Y_Z = int(str(get_distance_from_Y_Z['rows'][0]['elements'][0]['duration']['text']).split(' ')[0])
 
-        bus_reach_final_destination = (datetime.combine(date(1,1,1),time_bus_will_reach) + timedelta(minutes=duration_Y_Z)).time()
-        print("Time at which you'll reach your destination is {}".format(bus_reach_final_destination))
-        print('Total Distance user travel: {}'.format(distace_Y_Z))
-        data = {'bus_eta': format(ETA), 'time_to_destination': format(bus_reach_final_destination),
-                'total_distance': distace_Y_Z, 'user_get_in': Y, 'user_get_out': Z}
-        all_schedules.append(data)
+            bus_reach_final_destination = (
+            datetime.combine(date(1, 1, 1), time_bus_will_reach) + timedelta(minutes=duration_Y_Z)).time()
+            print("Time at which you'll reach your destination is {}".format(bus_reach_final_destination))
+            print('Total Distance user travel: {}'.format(str(get_distance_from_Y_Z['rows'][0]['elements'][0]['distance']['text'])))
+            print('***********************')
+            data = {'bus_eta': format(ETA), 'time_to_destination': format(bus_reach_final_destination),
+                    'total_distance': distace_Y_Z, 'user_get_in': Y, 'user_get_out': Z}
+            all_schedules.append(data)
 
-    with open('data.json', 'w') as fp:
-        json.dump(all_schedules, fp)
+        # with open('data.json', 'w') as fp:
+        #     json.dump(all_schedules, fp)
+        return all_schedules
+
+
+
+# USAGE
+# from MainUpdated import Routes
+# input_time = '07.01.2018 09:04:42' # MUST BE STRING AND FOLLOWS THIS FORMAT
+# input_stop = str('Turkman Gate').lower() # MUST BE STRING BUT CAN BE CASE INSENTIVE BECAUSE I AM USING LOWERCASE
+# input_final_stop = str('Ganesh Nagar').lower() # MUST BE STRING BUT CAN BE CASE INSENTIVE BECAUSE I AM USING LOWERCASE
+# number_of_buses = 1 # MUST BE INTEGER
+# 
+# obj = Routes(input_time, input_stop, input_final_stop, number_of_buses)
+# variables = obj.validate_variables()
+# result_data = obj.get_route_information(variables)
--- master/views.py
+++ master/views.py
@@ -3,7 +3,7 @@ import logging
 import traceback
 from django.http.response import HttpResponse
 import requests
-
+from main import Routes
 logger = logging.getLogger(__name__)
 
 
@@ -20,29 +20,41 @@ def home(request):
 
 def get_bus_status(request):
     print("get bus status called")
-    datetime = request.POST.get('datetime','')
-    neareststop = request.POST.get('neareststop','')
-    finalstop = request.POST.get('finalstop','')
+    input_time = request.POST.get('datetime','07.01.2018 09:04:42')
+    input_stop = request.POST.get('neareststop','turkman gate')
+    input_final_stop = request.POST.get('finalstop','ganesh nagar')
+    number_of_buses = 1
     # print 'datetime ='+str(request.POST['datetime'])
     # print 'datetime old ='+str(datetime)
+
+    obj = Routes(input_time, input_stop, input_final_stop, number_of_buses)
+    variables = obj.validate_variables()    
+    result_data = obj.get_route_information(variables)
+    print("result_data ="+result_data)
+
     data = {"status":1, 
-            "datetime" : datetime, 
-            "neareststop" : neareststop, 
-            "finalstop" : finalstop,
+            "datetime" : input_time, 
+            "neareststop" : input_stop, 
+            "finalstop" : input_final_stop,
             }
     data = json.dumps(data)
     return HttpResponse(data,content_type= 'application/json')
 
+
 def set_bus_location(request):
-    lat = request.POST.get('lat')
-    lng = request.POST.get('lng')
-    data = {'lat': lat, 'lng' : lng}
+    lat = request.POST.get('lat','')
+    lng = request.POST.get('lng','')
+    print('lat '+str(lat) + ' & lng '+str(lng))
+    data = {'lat':lat,
+            'lng':lng}
     with open("bus_location.json", "w") as jsonFile:
-    json.dump(data, jsonFile)
+        json.dump(data, jsonFile)
     sendData = {'status':'success'}
     return HttpResponse(data,content_type= 'application/json')
 
 def get_bus_location(request):
     with open("replayScript.json", "r") as jsonFile:
-    data = json.load(jsonFile)
-    return HttpResponse(data, content_type='application/json')
+        data = json.load(jsonFile)
+        return HttpResponse(data, content_type='application/json')
+
+
